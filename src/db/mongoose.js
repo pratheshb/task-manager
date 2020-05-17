f113e62d9358aa8aefff2957ce15ada15e581d9b
Baskaran <Prathesh.Baskaran@agilysys.com>
2020-05-17 12:46:34 +0530

    added database name in config

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
-const databaseName = 'task-manager-api';
-
 mongoose.connect(
-    process.env.MONGODB_URL + '/' + databaseName,
+    process.env.MONGODB_URL,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
